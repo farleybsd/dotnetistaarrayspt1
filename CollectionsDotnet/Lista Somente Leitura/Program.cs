b77c37bf327514e67b2c34a9f6fe2b145e40a52d
farleybsd <farley.t.i@hotmail.com>
2021-01-30 18:21:46 -0300

    Lista Somente Leitura

---
+++ CollectionsDotnet/Lista Somente Leitura/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_Somente_Leitura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharpCollection = new Curso("c#Collections","Marcelo Oliveira");
+            csharpCollection.Adicona(new Aula("Trabalhando com listas",21));
+            Imprimir(csharpCollection.Aulas);
+
+            Console.ReadKey();
+        }
+        
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            Console.Clear();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+    }
+}
