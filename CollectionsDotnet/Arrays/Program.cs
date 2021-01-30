bacbe037eb9f77d0148c17f3c5d3f30839d38c66
farleybsd <farley.t.i@hotmail.com>
2021-01-29 23:41:58 -0300

    Declarando e Populando Arrays

---
+++ CollectionsDotnet/Arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aulaIntro = "Introdução as coleções";
+            string AulaModelando = "Modelando a classe Aula";
+            string aulaSets = "Trabalhando com conjuntos";
+
+            string[] aulas = new string[]
+            {
+                aulaIntro,
+                AulaModelando,
+                aulaSets
+            };
+
+            string[] aulasDois = new string[3];
+            aulasDois[0] = aulaIntro;
+            aulasDois[1] = AulaModelando;
+            aulasDois[2] = aulaSets;
+
+            Imprimir(aulas);
+
+            Console.ReadKey();
+        }
+
+        private static void Imprimir(string[] aulas)
+        {
+            foreach (var item in aulas)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
