727802a2abf65abae7be9e72811f30fb23cf7370
farleybsd <farley.t.i@hotmail.com>
2021-01-30 17:07:10 -0300

    Operações com Listas

--- CollectionsDotnet/Listas/Program.cs
+++ CollectionsDotnet/Listas/Program.cs
@@ -37,6 +37,47 @@ namespace Listas
             Console.WriteLine("A ultima aula é " + aulasdois[aulasdois.Count - 1]);
             Console.WriteLine(" Exemplo Dois - A ultima aula é " + aulasdois.Last());
 
+            aulasdois[0] = "Trabalhando com Listas";
+            imprimir(aulasdois);
+
+            Console.WriteLine("A primeira aula 'Trabalhando' é :"
+                + aulasdois.Last(aula =>  aula.Contains("Trabalhando")));
+
+
+            Console.WriteLine("A ultima aula 'Trabalhando' é :"
+                + aulasdois.First(aula => aula.Contains("Trabalhando")));
+
+            Console.WriteLine("A primeira aula 'Conclusão' é :"
+                + aulasdois.FirstOrDefault(aula => aula.Contains("Conclusão")));
+
+            Console.WriteLine("Invertendo a ordem ");
+            aulasdois.Reverse();
+            imprimir(aulasdois);
+
+            Console.WriteLine("Remvendo um indice");
+            aulasdois.RemoveAt(aulas.Count - 1);
+            imprimir(aulasdois);
+
+            aulasdois.Add("Conclusao");
+            imprimir(aulasdois);
+
+            Console.WriteLine("Ordem Alfabetica");
+            aulasdois.Sort();
+            imprimir(aulasdois);
+
+
+            Console.WriteLine("Fazendo uma copia dos dois ultimos registros");
+
+            List<string> copia = aulasdois.GetRange(aulasdois.Count -2 ,2);
+            imprimir(copia);
+
+            Console.WriteLine("Fazendo um clone");
+            List<string> clone = new List<string>(aulasdois);
+            imprimir(clone);
+
+            Console.WriteLine("removendo dos dois ultimos itens");
+            clone.RemoveRange(clone.Count - 2,2);
+            imprimir(clone);
             Console.ReadKey();
 
         }
